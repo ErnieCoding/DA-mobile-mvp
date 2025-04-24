991fb0819c75e8a2a31a1bb0d22539c41f8d5d9e
Maceface2 <hmasonostman@gmail.com>
2025-04-24 13:00:10 -0400

    modified the whisper api to run locally commented out api call

--- backend/api/transcription.py
+++ backend/api/transcription.py
@@ -1,3 +1,20 @@
+#local whisper running with small model
+import whisper
+
+
+def transcribe_audio(audio_file_path):
+    print(f"Loading Whisper model...")
+    model = whisper.load_model("small")
+    
+    print(f"Transcribing audio file: {audio_file_path}")
+    try:
+        result = model.transcribe(audio_file_path)
+        return result["text"]
+    except Exception as e:
+        print(f"Error in transcription: {str(e)}")
+        return None 
+#api call 
+'''
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -18,4 +35,5 @@ def transcribe_audio(audio_file_path):
     #error if transcription is not working 
     except Exception as e:
         print(f"Error in transcription: {str(e)}")
-        return None 
+        return None
+'''
